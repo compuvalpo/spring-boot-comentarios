e3b4d7278adce7364f6b19bc33f1e7202fcc2bd7
Victor Retamal <compuvalpo@gmail.com>
2024-02-28 23:19:05 -0300

    Eliminar Comentario

--- comentario-front/src/app/comentario-editar/comentario-editar.component.ts
+++ comentario-front/src/app/comentario-editar/comentario-editar.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators,
+  FormGroup,
+} from '@angular/forms';
 import { Router, RouterModule, ActivatedRoute } from '@angular/router';
 import { ComentarioService } from '../services/comentario.service';
 import swal from 'sweetalert2';
@@ -10,9 +15,9 @@ import { Comentario } from '../interfaces/comentario.Interface';
   standalone: true,
   imports: [ReactiveFormsModule, RouterModule],
   templateUrl: './comentario-editar.component.html',
-  styleUrl: './comentario-editar.component.css'
+  styleUrl: './comentario-editar.component.css',
 })
-export default class ComentarioEditarComponent implements OnInit{
+export default class ComentarioEditarComponent implements OnInit {
   private formBuilder = inject(FormBuilder);
   private comentarioService = inject(ComentarioService);
   private router = inject(Router);
@@ -21,53 +26,50 @@ export default class ComentarioEditarComponent implements OnInit{
   form: FormGroup;
   comentario: Comentario;
 
-ngOnInit(): void {
+  ngOnInit(): void {
     const id = this.aRoute.snapshot.paramMap.get('id');
 
-    if (id){
-      this.comentarioService.get(parseInt(id)).subscribe(comentario => {
-
-      this.comentario = comentario;
+    if (id) {
+      this.comentarioService.get(parseInt(id)).subscribe((comentario) => {
+        this.comentario = comentario;
 
         this.form = this.formBuilder.group({
           email: [comentario.email, [Validators.required]],
           fk_region: [comentario.fk_region, [Validators.required]],
           comentario: [comentario.comentario, [Validators.required]],
         });
-
-        })
-    }else{
+      });
+    } else {
       this.form = this.formBuilder.group({
         email: ['', [Validators.required]],
         fk_region: ['', [Validators.required]],
         comentario: ['', [Validators.required]],
       });
     }
-}
+  }
 
   editar() {
     const comentario = this.form.value;
-console.log(comentario)
-      this.comentarioService.update(this.comentario.id,comentario).subscribe(() => {
+
+    this.comentarioService
+      .update(this.comentario.id, comentario)
+      .subscribe(() => {
         this.finalizarEditar();
       });
-
   }
 
-  finalizarEditar(){
-
-    swal.fire({
-      title: 'Comentario Editado',
-      text: 'Su comentario ha sido Editado',
-      icon: 'success',
-      confirmButtonText: 'OK',
-      timer: 2500,
-      timerProgressBar: true
-    }).then((result) => {
-
-      this.router.navigate(['/']);
-
-    });
+  finalizarEditar() {
+    swal
+      .fire({
+        title: 'Comentario Editado',
+        text: 'Su comentario ha sido Editado',
+        icon: 'success',
+        confirmButtonText: 'OK',
+        timer: 2500,
+        timerProgressBar: true,
+      })
+      .then((result) => {
+        this.router.navigate(['/']);
+      });
   }
-
 }
--- comentario-front/src/app/comentario-list/comentario-list.component.ts
+++ comentario-front/src/app/comentario-list/comentario-list.component.ts
@@ -3,23 +3,44 @@ import { ComentarioService } from '../services/comentario.service';
 import { DatePipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Comentario } from '../interfaces/comentario.Interface';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-comentario-list',
   standalone: true,
   imports: [DatePipe, RouterModule],
   templateUrl: './comentario-list.component.html',
-  styleUrl: './comentario-list.component.css'
+  styleUrl: './comentario-list.component.css',
 })
+
 export default class ComentarioListComponent implements OnInit {
   private comentarioService = inject(ComentarioService);
   comentarios: Comentario[] = [];
 
-  ngOnInit(): void {
-      this.comentarioService.list().subscribe(comentarios => {
+  recargar() {
+    this.comentarioService.list().subscribe((comentarios) => {
       this.comentarios = comentarios;
-      });
-
+    });
   }
 
+  ngOnInit(): void {
+    this.recargar();
+  }
+
+  deleteComentario(comentario: Comentario) {
+    this.comentarioService.delete(comentario.id).subscribe(() => {
+      swal
+        .fire({
+          title: 'Comentario Eliminado',
+          text: 'Su comentario ha sido eliminado',
+          icon: 'success',
+          confirmButtonText: 'OK',
+          timer: 2500,
+          timerProgressBar: true,
+        })
+        .then((result) => {
+          this.recargar();
+        });
+    });
+  }
 }
