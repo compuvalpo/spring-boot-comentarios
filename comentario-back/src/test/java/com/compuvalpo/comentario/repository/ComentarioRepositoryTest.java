990194ff5e540ac64782c671abaf8759d00d0768
Victor Retamal <compuvalpo@gmail.com>
2024-03-09 20:49:11 -0300

    Corrección de error

---
+++ comentario-back/src/test/java/com/compuvalpo/comentario/repository/ComentarioRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.compuvalpo.comentario.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComentarioRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
